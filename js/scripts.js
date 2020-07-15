06c10c879ef277046aade81212a19b55b2bf76a9
mustafaHassoon <M+mustafaHassoon@users.noreply.github.com>
2020-07-15 18:07:22 +0200

    deleting old code

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,4 @@
-// var giantsList= [
-//     { name:  'GQ Lupi b' ,type: 'exoplanet', distanceLY: 495, },
-//     { name:  'UY Scuti' ,type: 'star', distanceLY: 5100, },
-//     { name:  'The Tarantula Nebula' ,type: 'Nebula', distanceLY: 160, },
-//     { name:  'CMB Cold Spot' ,type: 'empty spot', distanceLY: 160, },
-//     { name:  'IC 1101' ,type: 'galaxy', distanceLY: 1045000000, },
-//     { name:  'TON 618' ,type: 'black hole', distanceLY: 10371774218, },
-//     { name:  'Fermi Bubbles' ,type: 'galactic farts', distanceLY: 26700, },
-//     { name:  'Protocluster SPT2349-56' ,type: 'single object', distanceLY:  12400000000, },
-//     { name:  'Shapley Supercluster' ,type: 'galactic collection', distanceLY:  652000000, },
-//     { name:  'Laniakea Supercluster' ,type: 'supercluster', distanceLY: 652000000,},
-//     { name:  'Huge-LQG' ,type: 'quasar collection', distanceLY:  9000000000,},
-//     { name:  'Hercules-Corona Borealis Great Wall' ,type: 'Largest thing', distanceLY: 10000000000,},
-// ];
 
-// // old for loop
-// // for ( let i = 0; i < giantsList.length; i++) { 
-
-// //         document.write('<h2>' + giantsList[i].name + '</h2>' + ' Distance in Light Year: ' + giantsList[i].distanceLY + ' away ' );
-   
-// //     if (giantsList[i].distanceLY >= 1000000) {
-// //          document.write(' (Wow that\'s too far!)' ) 
-// //     }
-// // }
-
-// function loopFunction(spaceObject){
-//     document.write(spaceObject.name + ' is a ' + spaceObject.type + '...' );
-// }
-// giantsList.forEach(loopFunction);
 var interstellarGiants = (function () {
     var giantsList = [
      { name:  'GQ Lupi b' ,type: 'exoplanet', distanceLY: 495, },
