1db39af6d9ad5b39d4b6daf78604acf720ed9e1a
mustafaHassoon <M+mustafaHassoon@users.noreply.github.com>
2020-10-07 13:25:22 +0200

    adding modal to the app

--- js/scripts.js
+++ js/scripts.js
@@ -81,7 +81,54 @@ var interstellarGiants = (function () {
     });
   }   
 
+ // Show modal content
+ var modalContainer = document.querySelector('#modal-container');
+ function showModal(title, text) {
+   modalContainer.innerHTML = '';
+   var modal = document.createElement('div');
+   modal.classList.add('modal');
 
+   var closeButtonElement = document.createElement('button');
+   closeButtonElement.classList.add('modal-close');
+   closeButtonElement.innerText = 'Close';
+   closeButtonElement.addEventListener('click', hideModal);
+
+   var titleElement = document.createElement('h1');
+   titleElement.innerText = title;
+
+   var contentElement = document.createElement('p');
+   contentElement.innerText = text;
+
+   modal.appendChild(closeButtonElement);
+   modal.appendChild(titleElement);
+   modal.appendChild(contentElement);
+   modalContainer.appendChild(modal);
+
+
+   modalContainer.classList.add('is-visible');
+ }
+
+ function hideModal() {
+   modalContainer.classList.remove('is-visible');
+ }
+
+ window.addEventListener('keydown', (e) => {
+   if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+     hideModal();  
+   }
+ });
+ modalContainer.addEventListener('click', (e) => {
+   // Since this is also triggered when clicking INSIDE the modal
+   // We only want to close if the user clicks directly on the overlay
+   var target = e.target;
+   if (target === modalContainer) {
+     hideModal();
+   }
+ });
+
+ document.querySelector('#show-modal').addEventListener('click', () => {
+   showModal('Modal title', 'This is the modal content!');
+ });
     return {
       add: add,
       getAll: getAll,
@@ -89,6 +136,8 @@ var interstellarGiants = (function () {
       addListItem: addListItem,
       showDetails: showDetails,
       loadDetails: loadDetails,
+      showModal: showModal,
+      hideModal: hideModal,
     };
 
     
