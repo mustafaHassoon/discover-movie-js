5642a0b8b25cb6dd7adff29b4e43dcc3ba9eeb19
mustafaHassoon <M+mustafaHassoon@users.noreply.github.com>
2020-10-08 04:50:39 +0200

    adding show modal function

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,17 @@
 //wraping the list inside IIFE
 var interstellarGiants = (function () { 
-    // var giantsList = [
-    //  { name:  'GQ Lupi b' ,type: 'exoplanet', distanceLY: 495, },
-    //  { name:  'UY Scuti' ,type: 'star', distanceLY: 5100, },
-    //  { name:  'The Tarantula Nebula' ,type: 'Nebula', distanceLY: 160, },
-    //  { name:  'CMB Cold Spot' ,type: 'empty spot', distanceLY: 160, },
-    //  { name:  'IC 1101' ,type: 'galaxy', distanceLY: 1045000000, },
-    //  { name:  'TON 618' ,type: 'black hole', distanceLY: 10371774218, },
-    //  { name:  'Fermi Bubbles' ,type: 'galactic farts', distanceLY: 26700, },
-    //  { name:  'Protocluster SPT2349-56' ,type: 'single object', distanceLY:  12400000000, },
-    //  { name:  'Shapley Supercluster' ,type: 'galactic collection', distanceLY:  652000000, },
-    //  { name:  'Laniakea Supercluster' ,type: 'supercluster', distanceLY: 652000000,},
-    //  { name:  'Huge-LQG' ,type: 'quasar collection', distanceLY:  9000000000,},
-    //  { name:  'Hercules-Corona Borealis Great Wall' ,type: 'Largest thing', distanceLY: 10000000000,},
-    // ];
-
+  
     var giantsList = [];
     var apiUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=ee0326c2a9a0b787ee75f169ae1003ff&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
-  //FUNCTION TO EXTRACT OBJECTS FROM THE LIST
-
+  
+    //FUNCTION TO EXTRACT OBJECTS FROM THE LIST
 
    function getAll() {
     return giantsList;
   }
 
-   function add(item) {
-      giantsList.push(item);
+   function add(pokemon) {
+      giantsList.push(pokemon);
     }
   
    function loadList() {
@@ -40,11 +26,13 @@ var interstellarGiants = (function () {
           posterUrl: item.poster_path
         };
         add(pokemon);
+        console.log(pokemon);
       });
     }).catch(function (e) {
       console.error(e);
     })
   } 
+  
 
     // FUNCTION TO ADD  NEW LISTITEM FOR EACH SPACE OBJECT 
     function addListItem (pokemon){
@@ -61,47 +49,70 @@ var interstellarGiants = (function () {
       });
     }
     //FUNCTION TO SHOW DETAILS OF THE LIST ITEM
-    function showDetails(pokemon) {
-      loadDetails(pokemon).then(function () {
-        console.log(pokemon);
+    function showDetails(item) {
+      interstellarGiants.loadDetails(item).then(function () {
+        console.log(item);
+        showModal(item);
       });
     }
 
+    function loadDetails2(item) {
+    
+        item.imageUrl = details.poster_path;
+        item.releaseDate = details.elease_date;
+        item.overview = details.overview;
+     
+    }   
    function loadDetails(item) {
     var url = apiUrl;
     return fetch(url).then(function (response) {
       return response.json();
     }).then(function (details) {
       // Now we add the details to the item
-      // item.imageUrl = details.poster_path;
-      // item.releaseDate = details.elease_date;
-      // item.overview = details.overview;
+      item.imageUrl = details.poster_path;
+      item.releaseDate = details.elease_date;
+      item.overview = details.overview;
     }).catch(function (e) {
       console.error(e);
     });
   }   
 
- // Show modal content
+ // Show modal function
+ 
  var modalContainer = document.querySelector('#modal-container');
- function showModal(title, text) {
+ function showModal(item) {
+   // Clear existing modal content
    modalContainer.innerHTML = '';
+   // Creating div element in DOM
    var modal = document.createElement('div');
+   // adding class to div DOM element
    modal.classList.add('modal');
-
+   // create closing button in modal content
    var closeButtonElement = document.createElement('button');
    closeButtonElement.classList.add('modal-close');
    closeButtonElement.innerText = 'Close';
+   // Add event listener to close modal when botton is clicked
    closeButtonElement.addEventListener('click', hideModal);
-
+   // Create element for title in modal content
    var titleElement = document.createElement('h1');
-   titleElement.innerText = title;
+   titleElement.innerText = item.title;
+   // Create element for release date in modal content
+   var releaseDate = document.createElement('p');
+   releaseDate.innerText = 'Release date : ' + item.release_date;
+  // Create element for overview in modal content
+   var overview = document.createElement('p');
+   overview.innerText = 'Overview : ' + item.overview;
 
-   var contentElement = document.createElement('p');
-   contentElement.innerText = text;
+   // Create img in modal content
+   var imageElement = document.createElement('img');
+   imageElement.classList.add('modal-img');
+   imageElement.setAttribute('src', item.poster_path);
 
    modal.appendChild(closeButtonElement);
    modal.appendChild(titleElement);
-   modal.appendChild(contentElement);
+   modal.appendChild(releaseDate);
+   modal.appendChild(overview);
+   modal.appendChild(imageElement);
    modalContainer.appendChild(modal);
 
 
